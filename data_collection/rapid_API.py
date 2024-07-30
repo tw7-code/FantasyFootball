50aab0443663127c97f04cbff98f8c3fecc34b92
tw7-code <tw7.edu@gmail.com>
2024-07-30 13:14:06 -0700

    Add API functions for sleeper and rapid API. Use gitignore to protect API keys from environment variables.

---
+++ data_collection/rapid_API.py
@@ -0,0 +1,425 @@
+import requests
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+def get_headers_and_host():
+
+    load_dotenv()
+    api_key = os.getenv('RAPID_API_KEY')
+    database_url = os.getenv('RAPID_DATABASE_URL')    
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": database_url
+    }
+    return headers
+
+
+def get_news(playerID, teamID, teamAbv, topNews, fantasyNews, recentNews, maxItems):
+    
+    database_name = 'getNFLNews'
+
+    querystring = {
+        "playerID": str(playerID),
+        "teamID": str(teamID),
+        "teamAbv": str(teamAbv),
+        "topNews": str(topNews),
+        "fantasyNews": str(fantasyNews),
+        "recentNews": str(recentNews),
+        "maxItems": str(maxItems)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_game_info(gameID):
+    
+    database_name = 'getNFLGameInfo'
+
+    querystring = {
+        'gameID': str(gameID)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_scores(gameDate, gameID, topPerformers, gameWeek, season, seasonType):
+    
+    database_name = 'getNFLScoresOnly'
+
+    querystring = {
+        "gameDate": str(gameDate),
+        "gameID": str(gameID),
+        "topPerformers": str(topPerformers),
+        "gameWeek": str(gameWeek),
+        "season": str(season),
+        "seasonType": str(seasonType)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_weekly_schedule(week, seasonType, season):
+    
+    database_name = 'getNFLGamesForWeek'
+
+    querystring = {
+        "week": str(week),
+        "seasonType": str(seasonType),
+        "season": str(season)
+    } 
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_team_schedule(teamID, teamAbv, season):
+    
+    database_name = 'getNFLTeamSchedule'
+
+    querystring = {
+        "teamID": str(teamID),
+        "teamAbv": str(teamAbv),
+        "season": str(season)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_gameday_schedule(gameDate):
+    
+    database_name = 'getNFLGamesForDate'
+
+    querystring = {
+        "gameDate": str(gameDate)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_box_score(gameID, playByPlay):
+    
+    database_name = 'getNFLBoxScore'
+
+    querystring = {
+        "gameID": str(gameID),
+        "playByPlay": str(playByPlay)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_fantasy_projections(week, season=2024, ppr=1.0, passTD=6.0):
+    
+    database_name = 'getNFLProjections'
+
+    querystring = {
+        "week": str(week),
+        "archiveSeason": str(season),
+        "pointsPerReception": str(ppr),
+        "passTD": str(passTD),
+        "passAttempts": "0.0",
+        "passCompletion": "0.0",
+        "carries": "0.0",
+        "targets": "0.0"
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_team_stats(season=2024):
+    
+    database_name = 'getNFLTeams'
+
+    querystring = {
+        "sortBy": "standings",
+        "rosters": "true",
+        "schedules": "true",
+        "topPerformers": "true",
+        "teamStats": "true",
+        "teamStatsSeason": str(season)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_ADP_data(adp_type='PPR', adpDate='today', pos=''):
+    
+    database_name = 'getNFLADP'
+
+    if adpDate == 'today':
+        adpDate = datetime.today().strftime('%Y%m%d')
+
+    querystring = {
+        'adpType': str(adp_type),
+        'adpDate': str(adpDate),
+        'pos': str(pos)
+    }
+
+    if pos == '':
+        querystring.pop('pos')
+
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_player_info(playerName='', playerID='', getStats='true'):
+    
+    database_name = 'getNFLPlayerInfo'
+
+    querystring = {
+        "playerName": str(playerName),
+        "playerID": str(playerID),
+        "getStats": str(getStats)
+    }
+
+    if playerName=='':
+        querystring.pop('playerName')
+    if playerID=='':
+        querystring.pop('playerID')
+
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_player_stats(playerID='', gameID='', numberOfGames=''):
+    
+    database_name = 'getNFLGamesForPlayer'
+
+    querystring = {
+        "playerID": str(playerID),
+        "gameID": str(gameID),
+        "numberOfGames": str(numberOfGames),
+        "pointsPerReception": str(ppr),
+        "passTD": str(passTD),
+        "passAttempts": "0.0",
+        "passCompletion": "0.0",
+        "carries": "0.0",
+        "targets": "0.0"
+    }
+
+    if playerID=='':
+        querystring.pop('playerID')
+    if gameID=='':
+        querystring.pop('gameID')
+    if numberOfGames=='':
+        querystring.pop('numberOfGames')
+
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_betting_odds(gameDate, gameID):
+    
+    database_name = 'getNFLBettingOdds'
+
+    querystring = {
+        "gameDate": str(gameDate),
+        "gameID": str(gameID)
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_team_roster(teamAbv, archiveDate='today'):
+    
+    database_name = 'getNFLTeamRoster'
+
+    if adpDate == 'today':
+        adpDate = datetime.today().strftime('%Y%m%d')
+
+    querystring = {
+        "teamAbv": str(teamAbv),
+        "archiveDate": str(season),
+        "getStats": "true"
+    }
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
+
+
+def get_player_list():
+    
+    database_name = 'getNFLPlayerList'
+
+    querystring = {}
+    headers = get_headers_and_host()
+    url = f'https://{headers['x-rapidapi-host']}/{database_name}'
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+        print()
+        print(querystring)
---
+++ data_collection/sleeper_API.py
@@ -0,0 +1,211 @@
+import requests
+import pandas as pd
+
+def fetch_all_players():
+    # WARNING: Only do this max once per day!
+
+    url = f'https://api.sleeper.app/v1/players/nfl'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        players_data = response.json()
+        return players_data
+    else:
+        print(f'Failed to retrieve league data: {response.status_code}')
+
+
+def get_league_info(league_id='1095093570517798912'):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        league_data = response.json()
+        return league_data
+    else:
+        print(f'Failed to retrieve league data: {response.status_code}')
+
+
+def get_league_rosters(league_id='1095093570517798912'):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/rosters'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        league_data = response.json()
+        return league_data
+    else:
+        print(f'Failed to retrieve roster data: {response.status_code}')
+
+
+def get_league_users(league_id='1095093570517798912'):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/users'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        league_data = response.json()
+        return league_data
+    else:
+        print(f'Failed to retrieve user data: {response.status_code}')
+
+
+def get_league_matchups(league_id='1095093570517798912', week=1):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/matchups/{week}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        matchup_data = response.json()
+        return matchup_data
+    else:
+        print(f'Failed to retrieve matchup data: {response.status_code}')
+
+def get_league_playoff_brackets(league_id='1095093570517798912'):
+
+    url_winners = f'https://api.sleeper.app/v1/league/{league_id}/winners_bracket'
+    url_losers  = f'https://api.sleeper.app/v1/league/{league_id}/losers_bracket'
+    
+    response_winners = requests.get(url_winners)
+    response_losers = requests.get(url_losers)
+
+    failed = False
+    if response_winners.status_code != 200:
+        print(f'Failed to retrieve winners bracket data: {response_winners.status_code}')
+        failed = True  
+    if response_losers.status_code != 200:
+        print(f'Failed to retrieve losers bracket data: {response_losers.status_code}')
+        failed = True
+    
+    if not failed:
+        winners_bracket_data = response_winners.json()
+        losers_bracket_data = response_losers.json()
+        return winners_bracket_data, losers_bracket_data
+    
+def get_league_transactions(league_id='1095093570517798912', round=1):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/transactions/{round}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        transaction_data = response.json()
+        return transaction_data
+    else:
+        print(f'Failed to retrieve transaction data: {response.status_code}')
+    
+def get_nfl_state(sport='nfl'):
+
+    url = f'https://api.sleeper.app/v1/state/{sport}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        state_data = response.json()
+        return state_data
+    else:
+        print(f'Failed to retrieve matchup data: {response.status_code}')
+    
+def get_league_traded_picks(league_id='1095093570517798912'):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/traded_picks'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        traded_picks_data = response.json()
+        return traded_picks_data
+    else:
+        print(f'Failed to retrieve traded picks data: {response.status_code}')
+    
+def get_user_drafts(user_id='', year=2024):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/traded_picks'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        traded_picks_data = response.json()
+        return traded_picks_data
+    else:
+        print(f'Failed to retrieve user draft data: {response.status_code}')
+
+def get_league_drafts(league_id='1095093570517798912', year=2024):
+
+    url = f'https://api.sleeper.app/v1/league/{league_id}/drafts'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        draft_data = response.json()
+        return draft_data
+    else:
+        print(f'Failed to retrieve league draft data: {response.status_code}')
+
+def get_draft(draft_id=''):
+
+    url = f'https://api.sleeper.app/v1/draft/{draft_id}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        draft_data = response.json()
+        return draft_data
+    else:
+        print(f'Failed to retrieve draft data: {response.status_code}')
+
+def get_draft_picks(draft_id=''):
+
+    url = f'https://api.sleeper.app/v1/draft/{draft_id}/picks'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        draft_pick_data = response.json()
+        return draft_pick_data
+    else:
+        print(f'Failed to retrieve draft pick data: {response.status_code}')
+
+def get_traded_draft_picks(draft_id=''):
+
+    url = f'https://api.sleeper.app/v1/draft/{draft_id}/traded_picks'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        traded_pick_data = response.json()
+        return traded_pick_data
+    else:
+        print(f'Failed to retrieve traded draft pick data: {response.status_code}')
+
+
+def get_trending_players(type='add', lookback_hours=24, limit=25):
+
+    url = f'https://api.sleeper.app/v1/players/nfl/trending/{type}?lookback_hours={lookback_hours}&limit={limit}'
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        player_trends_data = response.json()
+        return player_trends_data
+    else:
+        print(f'Failed to retrieve player trends data: {response.status_code}')
